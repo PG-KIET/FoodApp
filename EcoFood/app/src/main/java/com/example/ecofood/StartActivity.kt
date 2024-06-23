39e090457d1a640b7403db9d762eaedf1a1e329f
Henry <giakietpham.2003@gmail.com>
2024-06-23 21:37:12 +0700

    fix bug

--- EcoFood/app/src/main/java/com/example/ecofood/StartActivity.kt
+++ EcoFood/app/src/main/java/com/example/ecofood/StartActivity.kt
@@ -1,7 +1,9 @@
 package com.example.ecofood
 
+import android.content.Intent
 import android.os.Bundle
 import android.renderscript.ScriptGroup.Binding
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,6 +19,11 @@ class StartActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val loginButton: Button = findViewById(R.id.button2)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
