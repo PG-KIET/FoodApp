68c24a23ceadeaa3aba85299880d2ca3aa231169
Henry <giakietpham.2003@gmail.com>
2024-08-05 20:10:47 +0700

    Finish Login And Sign Up Admin

--- AdminEcoFood/app/build.gradle.kts
+++ AdminEcoFood/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services") // Áp dụng plugin google-services
 }
 
 android {
@@ -38,12 +39,13 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.database) // Thêm dependency cho Firebase Database
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
