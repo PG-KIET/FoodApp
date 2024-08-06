6437062a8d1621ec42585acd96f5bfa603032c0b
Henry <giakietpham.2003@gmail.com>
2024-08-06 22:48:28 +0700

    Login Google

--- AdminEcoFood/app/build.gradle.kts
+++ AdminEcoFood/app/build.gradle.kts
@@ -42,10 +42,12 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.play.services.auth)
+    implementation(libs.firebase.auth.ktx)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.auth)
-    implementation(libs.firebase.database) // Thêm dependency cho Firebase Database
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
